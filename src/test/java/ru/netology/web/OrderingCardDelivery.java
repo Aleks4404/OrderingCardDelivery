d206b037ac4831e20108266e51e59a881ffb18f4
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-27 15:25:16 +0400

    Добавлен тест с корректными данными в файл OrderingCardDelivery.java

--- src/test/java/ru/netology/web/OrderingCardDelivery.java
+++ src/test/java/ru/netology/web/OrderingCardDelivery.java
@@ -2,14 +2,14 @@ package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class OrderingCardDelivery {
     private String openUrl = "http://localhost:9999";
