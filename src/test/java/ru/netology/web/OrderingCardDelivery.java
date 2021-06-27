54518dbacd2e33bed549ebb812d4d9feef750d0e
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-27 17:02:45 +0400

    Проверка прохождение теста, что он не упадет при 11 сек.

--- src/test/java/ru/netology/web/OrderingCardDelivery.java
+++ src/test/java/ru/netology/web/OrderingCardDelivery.java
@@ -31,7 +31,7 @@ public class OrderingCardDelivery {
         form.$("[data-test-id=phone] input").setValue(validPhone);
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(7));
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
         $("[data-test-id='notification'] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + formatter.format(nextDate)));
     }
 }
